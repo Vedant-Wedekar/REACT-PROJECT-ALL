077cb74d2ef8b803d997a398b73429c6cdb5004f
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-08 23:35:52 +0530

    New Element

---
+++ Project-6/my-app/src/components/Footer.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import Q from '../assets/coff-footer.png'
+const Footer = () => {
+  return (
+    <div className='grid h-[600px] w-full bg-[#61292f] text-white border-2 border-[#ffffff]'>
+
+        <div className="p1 flex justify-center items-center border-2 border-[#ffffff]">
+
+<div className="grid">
+
+    <div className="flex justify-center items-center border-2 border-[#ffffff]">
+        <div className=""><img className='h-[100px]' src={Q}></img></div>
+<div className="lk h-[30px] w-[150px] rounded-2xl bg-[#FACB4C] flex justify-center items-center text-[#301611] font-extrabold text-sm"> ORDER COFFEE &gt;</div>
+       
+       </div> 
+      <div className="border-2 border-[#ffffff]">    <div className="hs text-4xl flex gap-2"> <svg xmlns="http://www.w3.org/2000/svg" height={33} viewBox="0 0 512 512"><path fill="#ffffff" d="M88 0C74.7 0 64 10.7 64 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C120.5 112.3 128 119.9 128 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C119.5 47.7 112 40.1 112 24c0-13.3-10.7-24-24-24zM32 192c-17.7 0-32 14.3-32 32L0 416c0 53 43 96 96 96l192 0c53 0 96-43 96-96l16 0c61.9 0 112-50.1 112-112s-50.1-112-112-112l-48 0L32 192zm352 64l16 0c26.5 0 48 21.5 48 48s-21.5 48-48 48l-16 0 0-96zM224 24c0-13.3-10.7-24-24-24s-24 10.7-24 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C232.5 112.3 240 119.9 240 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C231.5 47.7 224 40.1 224 24z"/></svg> AWAKEN</div>
+      </div>
+        
+   
+    <div className="">
+        <div className=""></div><div className=""></div>
+    </div>
+</div>
+
+<div className="grid ">
+    <div className="border-2 border-[#ffffff] w-[700px] h-[250px] flex justify-between items-center "> 
+    <a>Shop</a>
+    <a>About Us</a>
+    <a>Our Stories</a>
+    <a>Voucher</a>
+     </div>
+    <div className="border-2 border-[#ffffff]w-[700px] h-[150px] flex justify-between items-center "> 
+
+    <a>All Product</a>
+    <a>Instant</a>
+    <a>Capsules</a>
+    <a>Ground</a>
+
+    </div>
+</div>
+
+
+
+        </div>
+
+
+
+
+
+        <div className="p2 flex justify-center items-center">
+            <div className="text-white">
+                <a>Terms</a>
+                <a>Privacy Policy</a>
+
+            </div>
+            <div className="">©2025Vedant-LTT</div>
+            <div className="">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M194.4 211.7a53.3 53.3 0 1 0 59.3 88.7 53.3 53.3 0 1 0 -59.3-88.7zm142.3-68.4c-5.2-5.2-11.5-9.3-18.4-12c-18.1-7.1-57.6-6.8-83.1-6.5c-4.1 0-7.9 .1-11.2 .1c-3.3 0-7.2 0-11.4-.1c-25.5-.3-64.8-.7-82.9 6.5c-6.9 2.7-13.1 6.8-18.4 12s-9.3 11.5-12 18.4c-7.1 18.1-6.7 57.7-6.5 83.2c0 4.1 .1 7.9 .1 11.1s0 7-.1 11.1c-.2 25.5-.6 65.1 6.5 83.2c2.7 6.9 6.8 13.1 12 18.4s11.5 9.3 18.4 12c18.1 7.1 57.6 6.8 83.1 6.5c4.1 0 7.9-.1 11.2-.1c3.3 0 7.2 0 11.4 .1c25.5 .3 64.8 .7 82.9-6.5c6.9-2.7 13.1-6.8 18.4-12s9.3-11.5 12-18.4c7.2-18 6.8-57.4 6.5-83c0-4.2-.1-8.1-.1-11.4s0-7.1 .1-11.4c.3-25.5 .7-64.9-6.5-83l0 0c-2.7-6.9-6.8-13.1-12-18.4zm-67.1 44.5A82 82 0 1 1 178.4 324.2a82 82 0 1 1 91.1-136.4zm29.2-1.3c-3.1-2.1-5.6-5.1-7.1-8.6s-1.8-7.3-1.1-11.1s2.6-7.1 5.2-9.8s6.1-4.5 9.8-5.2s7.6-.4 11.1 1.1s6.5 3.9 8.6 7s3.2 6.8 3.2 10.6c0 2.5-.5 5-1.4 7.3s-2.4 4.4-4.1 6.2s-3.9 3.2-6.2 4.2s-4.8 1.5-7.3 1.5l0 0c-3.8 0-7.5-1.1-10.6-3.2zM448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM357 389c-18.7 18.7-41.4 24.6-67 25.9c-26.4 1.5-105.6 1.5-132 0c-25.6-1.3-48.3-7.2-67-25.9s-24.6-41.4-25.8-67c-1.5-26.4-1.5-105.6 0-132c1.3-25.6 7.1-48.3 25.8-67s41.5-24.6 67-25.8c26.4-1.5 105.6-1.5 132 0c25.6 1.3 48.3 7.1 67 25.8s24.6 41.4 25.8 67c1.5 26.3 1.5 105.4 0 131.9c-1.3 25.6-7.1 48.3-25.8 67z"/></svg>
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
+
+
+
+
+
+
