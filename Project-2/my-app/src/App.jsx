0171c8161b78d1fb07de048340b92000deb9de3a
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-10 08:30:18 +0530

    font and importing components

--- Project-2/my-app/src/App.jsx
+++ Project-2/my-app/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Header from './component/Header'
+import Intro from './component/Intro'
+import Flami from './component/Flamigo'
 const App = () => {
   return (
     <div>
       <Header/>
+      <Intro
+      />
+      <Flami
+      />
     </div>
   )
 }
