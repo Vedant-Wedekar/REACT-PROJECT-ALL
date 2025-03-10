a95d23b6a0540d4d7754636e94a38c6377c80940
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-10 20:50:52 +0530

    NEW COMPONENT

---
+++ Project-2/my-app/src/component/Flamigo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Flamigo = () => {
+  return (
+    <div className='flex justify-evenly items-center w-full h-[500px] '>
+      
+    </div>
+  )
+}
+
+export default Flamigo
