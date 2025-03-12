47eccd4b8533b81d108a51e151d941d9620b790e
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-12 14:55:54 +0530

    little changes

--- Project-2/my-app/src/component/Intro.jsx
+++ Project-2/my-app/src/component/Intro.jsx
@@ -3,6 +3,8 @@ import BG from '../assets/BG4.jpg'
 
 const Intro = () => {
   return (
+    
+    
     <div className='flex justify-evenly items-center w-full h-screen '>
 
 <div className="bg-[url(assets/BG4.jpg)] mt-[80px] h-[700px] rounded-4xl bg-cover  w-[1450px] ">
@@ -12,13 +14,15 @@ const Intro = () => {
         <div className="text-white w-[100px] flex justify-center items-center h-[40px] text-l  mx-8 my-15 border-white border-1 rounded-4xl">lorem</div>
 
         <div className="grid">
-            <div className="text-8xl text-white">Explore The<br></br> World With Us </div>
+            <div className="text-7xl text-white mt-[100px] ml-[70px]">Explore The<br></br> World With Us </div>
             <div className=""></div>
         </div>
     </div>
 
 
-    <div className=""></div>
+    <div className="w-30 h-30  backdrop-blur-lg border-2 border-white blur-sm rounded-full"></div>
+    <div className="w-30 h-30  rounded-full transform translate-y-[-45px] border-2 border-white"></div>
+
 </div>
 
 
@@ -26,7 +30,6 @@ const Intro = () => {
 </div>
       {/* <img src={BG} className='  w-[1450px]  mt-[140px] h-[800px] rounded-4xl' ></img> */}
 
-      <div className="pol h-[100px] w-[100px]"></div>
     </div>
   )
 }
