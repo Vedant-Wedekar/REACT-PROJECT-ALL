f9748edb83ba681638d2db8fb799d62afcbc4759
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-05-01 22:37:53 +0530

    feat: update Hv component layout and styling for Mother's Day promotion

--- Project-8/my-app/src/App.jsx
+++ Project-8/my-app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Hv from './components/Hv'
+import Product from './components/Product'
 const App = () => {
   return (
     <div>
       <Header />
       <Hv />
+      <Product />
+
     </div>
   )
 }
--- Project-8/my-app/src/components/Hv.jsx
+++ Project-8/my-app/src/components/Hv.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import poki from '../assets/poki.mp4'
 const Hv = () => {
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex justify-between items-center'>
       <video className=' ' src={poki} autoPlay loop muted></video>
-      <div className=" grid center">
-        <h1>WOMEN</h1>
-        <div className="">MOTHER'S DAY</div>
-        <div className="">Discover The Selection</div> 
+      <div className="grid place-items-center transform -translate-x- -translate-y-1/2 absolute top-5/8 left-6/8  ml-15 text-center gap-2">
+        <h1 className='text-[12px]'>WOMEN</h1>
+        <div className="text-[25px] font-medium ">Mother's Day</div>
+        <div className="underline">Discover The Selection</div> 
       </div>
     </div>
   )
