720e706d2254e01877a94546f0a558b642661e00
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-05-01 13:42:30 +0530

    feat: add initial project structure with React, Vite, and Tailwind CSS setup

---
+++ Project-8/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Hv from './components/Hv'
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hv />
+    </div>
+  )
+}
+
+export default App
