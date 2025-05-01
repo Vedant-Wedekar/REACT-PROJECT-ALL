55f7e0bfad37d4f5dc19b383c9e3750deee18560
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-05-01 22:38:13 +0530

    feat: add Product component to showcase a selection of creations

---
+++ Project-8/my-app/src/components/Product.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div className="">
+    <div className="flex  justify-center items-center mt-50 text-3xl font-medium">
+      <h1>Explore a Selection of the Maison's Creations</h1>
+</div>
+      <div className="flex justify-evenly items-center">
+
+        <div className="">
+            <img src="" alt="" />
+            <h1>Women's Handbags</h1>
+        </div>
+
+        <div className="">
+            <img src="" alt="" />
+            <h1>COCO MADEMOISELLE</h1>
+        </div>      
+
+        <div className="">
+            <img src="" alt="" />
+            <h1>COCO MADEMOISELLE</h1>
+        </div>      
+
+        <div className="">
+            <img src="" alt="" />
+            <h1>COCO MADEMOISELLE</h1>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Product
